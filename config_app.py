c601408e1b1efa8ac60bde8202df5f2b070d66d4
nurdin <44416234+nurdin0479@users.noreply.github.com>
2025-06-21 01:39:57 +0000

    config

---
+++ config_app.py
@@ -0,0 +1,13 @@
+import pytz
+
+# Timezone
+wib = pytz.timezone('Asia/Jakarta')
+
+
+
+
+
+TELEGRAM = {
+    "token": "7864711273:AAE0TZE93sB_HLkeJUpqJOv4qenrGIkGXy4",
+    "chat_id": "-1002798615104"
+}
